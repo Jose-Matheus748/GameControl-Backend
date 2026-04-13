e6a502fd0be0556520de2f3e985fe144a2d59773
Jose-Matheus748 <josemathaguiar@gmail.com>
2026-04-13 17:33:03 -0300

    Feat: Adicionando metodos para atualizar informações do usuário

--- src/main/java/com/gamecontrol/controller/UserController.java
+++ src/main/java/com/gamecontrol/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,5 +47,14 @@ public class UserController {
     @GetMapping("/{id}")
     public ResponseEntity<UserDTO> buscarPorId(@PathVariable String id) {
         return ResponseEntity.ok(userService.buscarUsuarioPorId(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDTO> atualizarUsuario(
+            @PathVariable String id,
+            @RequestBody UserDTO usuarioAtualizado
+    ) {
+        UserDTO atualizado = userService.atualizarUsuario(id, usuarioAtualizado);
+        return ResponseEntity.ok(atualizado);
     }
 }
